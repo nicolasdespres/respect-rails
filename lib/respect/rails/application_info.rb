ff2df80d1f54b0f4bcee46ba1f2bb678fc717663
Nicolas Despres <nicolas.despres@gmail.com>
2013-04-23 21:32:20 +0200

    Explicitly asked for a class to build ApplicationInfo.

--- lib/respect/rails/application_info.rb
+++ lib/respect/rails/application_info.rb
@@ -2,18 +2,18 @@ module Respect
   module Rails
     class ApplicationInfo < EngineInfo
 
-      def initialize(app)
-        super(app.class)
-        unless app.is_a?(::Rails::Application)
-          raise "'#{app}' must be a rails application."
+      def initialize(app_class)
+        super(app_class)
+        unless app_class < ::Rails::Application
+          raise "'#{app_class}' must be an ancestor of ::Rails::Application."
         end
-        @app = app
+        @app_class = app_class
       end
 
-      attr_reader :app
+      attr_reader :app_class
 
       def name
-        @app.class.parent_name.underscore
+        @app_class.parent_name.underscore
       end
 
     end # class ApplicationInfo
--- test/application_info_test.rb
+++ test/application_info_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ApplicationInfoTest < ActiveSupport::TestCase
   def setup
-    @app = Respect::Rails::ApplicationInfo.new(::Rails.application)
+    @app = Respect::Rails::ApplicationInfo.new(::Rails.application.class)
   end
 
   def test_name
