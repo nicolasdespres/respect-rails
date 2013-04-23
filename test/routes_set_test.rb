d9a4207178617247ef44d06ab3d2d415b2c68f1a
Nicolas Despres <nicolas.despres@gmail.com>
2013-04-23 12:17:28 +0200

    Factor RoutesSet instantiation.

--- test/routes_set_test.rb
+++ test/routes_set_test.rb
@@ -2,19 +2,22 @@ require "test_helper"
 
 class RoutesSetTest < ActiveSupport::TestCase
 
+  def setup
+    @routes = Respect::Rails::RoutesSet.new
+  end
+
   def test_engine_route_are_prefixed
-    assert(Respect::Rails::RoutesSet.new.any?{|r| r.path =~ %r{^/rest_spec/doc} })
+    assert(@routes.any?{|r| r.path =~ %r{^/rest_spec/doc} })
   end
 
   def test_app_name
-    routes_set = Respect::Rails::RoutesSet.new
-    assert(Respect::Rails.application_name == routes_set.app_name)
+    assert(Respect::Rails.application_name == @routes.app_name)
   end
 
   def test_app_routes_are_stored_in_engine_named_after_app
-    routes_set = Respect::Rails::RoutesSet.new
-    assert(routes_set.engines.has_key?(routes_set.app_name))
-    assert(routes_set.routes == routes_set.engines[routes_set.app_name])
+    assert(@routes.engines.has_key?(@routes.app_name))
+    assert(@routes.routes == @routes.engines[@routes.app_name])
   end
+
 
 end
