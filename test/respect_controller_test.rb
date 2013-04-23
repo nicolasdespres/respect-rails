ca0817639c3b8e1c88a90eced5b416d000a7017a
Nicolas Despres <nicolas.despres@gmail.com>
2013-04-23 11:53:47 +0200

    Add a FIXME.

--- test/respect_controller_test.rb
+++ test/respect_controller_test.rb
@@ -4,5 +4,7 @@ class RespectControllerTest < ActionController::TestCase
   def test_should_get_index
     get :index, use_route: :respect
     assert_response :success
+    # FIXME(Nicolas Despres): Test view using assert_select once we
+    # have restructured the HTML more cleanly.
   end
 end
