fa1a2ba280f8f3a873081532bc2d68fdb85f3d58
Nicolas Despres <nicolas.despres@gmail.com>
2013-04-24 10:40:33 +0200

    Add first integration test.

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -3,8 +3,17 @@ require 'test_helper'
 class NavigationTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  # FIXME(Nicolas Despres): Replace this test by lighter routes assertion
+  # when we succeed to make them works on an isolated engine (the use_route
+  # option does not seems to work here).
+  test "get the full API doc" do
+    get "/rest_spec"
+    assert_response :success
 
+    get "/rest_spec/doc"
+    assert_response :success
+
+    get "/rest_spec/doc.html"
+    assert_response :success
+  end
+end
