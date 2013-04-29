18ee6cb87d6d5f0e226b09139f3665bb134aa131
Nicolas Despres <nicolas.despres@gmail.com>
2013-04-29 11:45:14 +0200

    Prefer ruby helpers to plain HTML code.

--- app/helpers/respect/rails/application_helper.rb
+++ app/helpers/respect/rails/application_helper.rb
@@ -2,7 +2,11 @@ module Respect
   module Rails
     module ApplicationHelper
       def highlight_json_schema(json_schema)
-        "<pre><code>#{JSON.pretty_generate(json_schema)}</pre></code>".html_safe
+        content_tag(:pre) do
+          content_tag(:code) do
+            JSON.pretty_generate(json_schema).html_safe
+          end
+        end
       end
     end
   end
