57767f0d1b9c435d8b6dd2708adce96067162b1f
Nicolas Despres <nicolas.despres@gmail.com>
2013-04-29 11:43:43 +0200

    Add #highlight_json_schema helper.

--- app/helpers/respect/rails/application_helper.rb
+++ app/helpers/respect/rails/application_helper.rb
@@ -1,6 +1,9 @@
 module Respect
   module Rails
     module ApplicationHelper
+      def highlight_json_schema(json_schema)
+        "<pre><code>#{JSON.pretty_generate(json_schema)}</pre></code>".html_safe
+      end
     end
   end
 end
