b7a531db52af8a5f53b8c45e3db9cf4305d45cb3
Nicolas Despres <nicolas.despres@gmail.com>
2013-04-29 17:20:52 +0200

    Add generated address_book rails example app.

---
+++ examples/address_book/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressBook::Application.config.secret_token = 'be085b7fd4936ff9959ae77432ca96652a63d1da0e02d532c1ecbd9ba469911d426ae5e833da76e62fc55a5ff87f658f7fd9cd2e068d2cff5dc51f66fca47a03'
