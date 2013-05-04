08be9770a340743d5ca0e17ed957e2928e118537
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-04 20:30:16 +0200

    Fix comment.

--- test/skipped_automatic_validation_controller_test.rb
+++ test/skipped_automatic_validation_controller_test.rb
@@ -4,7 +4,7 @@ class SkippedAutomaticValidationControllerTest < ActionController::TestCase
 
   def test_basic_get_works
     assert_raises(RuntimeError) do
-      # The schema say we 42 is expected for param1, not 51.
+      # The schema say that 42 is expected for param1, not 51.
       get :basic_get, format: 'json', param1: 51
     end
   end
