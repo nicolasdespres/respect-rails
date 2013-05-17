7323925f528f80845eec7ad53a98afbc2ad34d6d
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-17 12:48:36 +0200

    Set application documentation.

---
+++ examples/address_book/config/initializers/respect.rb
@@ -0,0 +1,7 @@
+Respect::Rails.setup do |config|
+  config.app_documentation <<-EOS
+    The greatest address book out there.
+
+    This application let you store your contacts information on a server database.
+  EOS
+end
