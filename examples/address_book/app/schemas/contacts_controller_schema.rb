1acd64cae800f7e780c43185550282f5f641b72d
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-17 08:46:06 +0200

    Adjust to new DSL syntax.

--- examples/address_book/app/schemas/contacts_controller_schema.rb
+++ examples/address_book/app/schemas/contacts_controller_schema.rb
@@ -1,7 +1,7 @@
 class ContactsControllerSchema < ApplicationControllerSchema
   def create
-    request do
-      body_parameters do |s|
+    request do |r|
+      r.body_parameters do |s|
         s.hash "contact" do |s|
           s.contact_attributes
         end
@@ -9,8 +9,8 @@ class ContactsControllerSchema < ApplicationControllerSchema
     end
     response_for do |status|
       status.ok # contacts/create.schema
-      status.unprocessable_entity do
-        body do |s|
+      status.unprocessable_entity do |s|
+        s.body do |s|
           s.string "error"
         end
       end
