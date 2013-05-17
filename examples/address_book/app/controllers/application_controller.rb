1ee6a808cf5eb36901bc67718bd4ee5a00b0fa58
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-17 17:08:01 +0200

    Use request validation error handler.

--- examples/address_book/app/controllers/application_controller.rb
+++ examples/address_book/app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   around_filter :validate_schemas
+
+  rescue_from_request_validation_error if Rails.env.development?
 end
