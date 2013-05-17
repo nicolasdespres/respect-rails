076cb4c169279b22df418c6490a858149910cbf6
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-17 11:54:42 +0200

    Document contacts create method.

--- examples/address_book/app/schemas/contacts_controller_schema.rb
+++ examples/address_book/app/schemas/contacts_controller_schema.rb
@@ -1,5 +1,12 @@
 class ContactsControllerSchema < ApplicationControllerSchema
   def create
+    doc <<-EOS.strip_heredoc
+      Create a new contact in the address book.
+
+      This request create a new contact in the database with all the
+      attributes provided and respond its full description including
+      some more attributes.
+    EOS
     request do |r|
       r.body_parameters do |s|
         s.hash "contact" do |s|
