e789618d7f61e2235a06f43ce9358003fc7855a4
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-18 12:14:36 +0200

    Get rid of SchemasControllerSchema.

--- app/controllers/respect/rails/schemas_controller.rb
+++ app/controllers/respect/rails/schemas_controller.rb
@@ -3,9 +3,25 @@ module Respect
     # Controller to get all the information relative to the
     # REST API of this application.
     class SchemasController < ActionController::Base
+      def_action_schema :index do |s|
+        s.request do |r|
+          r.path_parameters do |s|
+            s.string "format", in: %w{html json}
+          end
+        end
+      end
+
       def index
         respond_to do |format|
           format.html # index.html.erb
+        end
+      end
+
+      def_action_schema :doc do |s|
+        s.request do |r|
+          r.path_parameters do |s|
+            s.string "format", in: %w{html json}
+          end
         end
       end
 
