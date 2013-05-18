d0547eedd582fcd3cd405205d728fe624759fa2f
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-18 15:01:58 +0200

    Document contacts attributes.

--- examples/address_book/app/helpers/respect/application_macros.rb
+++ examples/address_book/app/helpers/respect/application_macros.rb
@@ -5,18 +5,27 @@ module Respect
     end
 
     def contact_attributes
+      doc <<-EOS.strip_heredoc
+        The name of the contact.
+
+        First name and last name are separated by white space.
+        EOS
       string "name"
+      doc "How old is the contact."
       integer "age"
-      uri "homepage"
+      uri "homepage", doc: "The URL of the contact's homepage"
     end
 
     def record_timestamps
+      doc "When this record has been created."
       datetime "created_at"
+      doc "When this record has been updated lastly."
       datetime "updated_at"
     end
 
     def contact(name = "contact")
       hash name do |s|
+        doc "The identifier of this contact."
         s.id
         s.contact_attributes
         s.record_timestamps
