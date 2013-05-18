80a572b44a1962460db92faabdd38c2dd8115eb4
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-18 12:07:25 +0200

    Move has_schema test to new ActionSchema.

--- test/old_action_schema_test.rb
+++ test/old_action_schema_test.rb
@@ -2,9 +2,4 @@ require 'test_helper'
 
 class OldActionSchemaTest < ActiveSupport::TestCase
 
-  test "has_schema" do
-    assert(!Respect::Rails::OldActionSchema.from_controller(:automatic_validation, :no_schema_at_all).has_schema?)
-    assert(Respect::Rails::OldActionSchema.from_controller(:automatic_validation, :response_schema_from_file).has_schema?)
-    assert(Respect::Rails::OldActionSchema.from_controller(:automatic_validation, :response_schema_from_file_unknown_status).has_schema?)
-  end
 end
