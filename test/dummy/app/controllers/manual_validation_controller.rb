736093939df284e44828bfb335a5c6b83a27cc29
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-18 11:32:18 +0200

    Move 'raise_custom_error' spec into controller.

--- test/dummy/app/controllers/manual_validation_controller.rb
+++ test/dummy/app/controllers/manual_validation_controller.rb
@@ -1,6 +1,23 @@
 # An example of manual validation without using any filter.
 class ManualValidationController < ApplicationController
   # GET /manual_validation/raise_custom_error.json
+  def_action_schema :raise_custom_error do |s|
+    s.request do |r|
+      r.query_parameters do |s|
+        s.integer "param1", equal_to: 42
+      end
+    end
+    s.response_for do |status|
+      status.ok do |r|
+        r.body hash: false do |s|
+          s.hash do |s|
+            s.integer "id", equal_to: 53
+          end
+        end
+      end
+    end
+  end
+
   def raise_custom_error
     begin
       request.validate_schema
