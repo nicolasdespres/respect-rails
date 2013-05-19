bb8344fbcf580ec4515866d4121e5f47b401b22d
Nicolas Despres <nicolas.despres@gmail.com>
2013-05-19 23:16:01 +0200

    Describe headers more gently.

--- app/helpers/respect/rails/schemas_helper.rb
+++ app/helpers/respect/rails/schemas_helper.rb
@@ -6,6 +6,35 @@ module Respect
         Respect::JSONSchemaHTMLFormatter.new(json_schema).dump.html_safe
       end
 
+      def describe_option(name, value)
+        result = describe_option_internal(name, value)
+        result.html_safe if result
+      end
+
+      private
+
+      def describe_option_internal(name, value)
+        case name
+        when :doc, :required
+          nil
+        when :default
+          if value.nil?
+            nil
+          else
+            "default to #{value.inspect}"
+          end
+        when :equal_to
+          "Must be equal to #{value.inspect}"
+        when :allow_nil
+          if value
+            "May be null"
+          else
+            nil
+          end
+        else
+          "#{name}: #{value.inspect}"
+        end
+      end
     end
   end
 end
