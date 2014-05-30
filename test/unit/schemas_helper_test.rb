2d19e3734c730e1e9ec91d9205b6f2775d888011
Chi-Ting Leung <chiting.leung@gmail.com>
2014-05-30 23:54:05 +0100

    Add SchemasHelper tests

--- test/unit/schemas_helper_test.rb
+++ test/unit/schemas_helper_test.rb
@@ -1,21 +1,27 @@
 require "test_helper"
 
 class SchemasHelperTest < Test::Unit::TestCase
-  def test_describe_option
-    schemas = Object.new
-    schemas.extend(Respect::Rails::SchemasHelper)
+  def setup
+    @schemas = Object.new
+    @schemas.extend(Respect::Rails::SchemasHelper)
+  end
 
-    assert_nil schemas.describe_option(:doc, "value")
-    assert_nil schemas.describe_option(:required, true)
-    assert_nil schemas.describe_option(:default, nil)
-    assert_equal schemas.describe_option(:default, 42), "default to 42"
-    assert_equal schemas.describe_option(:equal_to, 42), "Must be equal to 42"
-    assert_equal schemas.describe_option(:allow_nil, 42), "May be null"
-    assert_nil schemas.describe_option(:allow_nil, nil)
-    assert_equal schemas.describe_option(:strict, 42), "Must contains exactly these parameters"
-    assert_nil schemas.describe_option(:strict, nil)
-    assert_equal schemas.describe_option(:greater_than, 42), "Must be greater than 42"
-    assert_equal schemas.describe_option(:non_existent_option, 42), "non_existent_option: 42"
-    assert schemas.describe_option(:default, "<script>42</script>").html_safe?, "Respect::Rails::SchemasHelper#describe_option return's is not HTML safe."
+  def test_describe_option
+    assert_nil @schemas.describe_option(:doc, "value")
+    assert_nil @schemas.describe_option(:required, true)
+    assert_nil @schemas.describe_option(:default, nil)
+    assert_equal @schemas.describe_option(:default, 42), "default to 42"
+    assert_equal @schemas.describe_option(:equal_to, 42), "Must be equal to 42"
+    assert_equal @schemas.describe_option(:allow_nil, 42), "May be null"
+    assert_nil @schemas.describe_option(:allow_nil, nil)
+    assert_equal @schemas.describe_option(:strict, 42), "Must contains exactly these parameters"
+    assert_nil @schemas.describe_option(:strict, nil)
+    assert_equal @schemas.describe_option(:greater_than, 42), "Must be greater than 42"
+    assert_equal @schemas.describe_option(:non_existent_option, 42), "non_existent_option: 42"
+    assert @schemas.describe_option(:default, "<script>42</script>").html_safe?, "Respect::Rails::SchemasHelper#describe_option return's is not HTML safe."
+  end
+
+  def test_build_parameter_name
+    assert_equal @schemas.build_parameter_name([:test, :parameters, 42]), "test[parameters][42]"
   end
 end
